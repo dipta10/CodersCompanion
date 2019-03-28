3e24ceb7380c3df68eead0268bf97123a8a66912
dipta10 <dip9623@yahoo.com>
2019-03-28 06:27:55 +0600

    added Markdown support. removed showing dashbhboard issue without being logged in. just imported the auth.

--- src/Components/posts/CreatePost.js
+++ src/Components/posts/CreatePost.js
@@ -1,8 +1,13 @@
-import React, { Component } from "react";
-import { connect } from 'react-redux'
-import { createProject } from "../../store/actions/projectActions";
-import { Redirect } from 'react-router-dom'
+import React, {Component} from "react";
+import {connect} from 'react-redux'
+import {createProject} from "../../store/actions/projectActions";
+import {Redirect} from 'react-router-dom'
+import MarkdownRenderer from 'react-markdown-renderer';
 import {keyword, linkurl} from "../../keyword";
+import TextareaAutosize from 'react-textarea-autosize';
+import { Form, TextArea } from 'semantic-ui-react'
+
+
 
 export class CreatePost extends Component {
   state = {
@@ -19,31 +24,54 @@ export class CreatePost extends Component {
     this.props.createProjectActionType(this.state);
     this.props.history.push(linkurl.dashboard);
   };
+
   render() {
 
     const auth = this.props.auth;
-    if (!auth) return <Redirect to='/signin'/>;
+    if (auth.uid == null) return <Redirect to='/signin'/>;
 
     return (
-      <div className="container">
-        <form onSubmit={this.handleSubmit} className="white">
-          <h5 className="grey-text text-darken-3">Create New Post</h5>
-          <div className="input-field">
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" onChange={this.handleChange} />
-          </div>
-          <div className="input-field">
-            <label htmlFor="password">Post Content</label>
-            <textarea
-              className="materialize-textarea"
-              id="content"
-              onChange={this.handleChange}
-            />
+      <div className="">
+        <Form onSubmit={this.handleSubmit} className="white ">
+          <h5 className="grey-text text-darken-3 center">Create New Post</h5>
+          <div className="row">
+            <div className="col s12 m6">
+              <div className="input-field">
+                <label htmlFor="title">Title</label>
+                <input type="text" id="title" onChange={this.handleChange}/>
+              </div>
+              <div className="input-field">
+                {/*<label htmlFor="">Post Content</label>*/}
+                <textarea
+                  className="materialize-textarea"
+                  rows={100}
+                  cols={100}
+                  aria-expanded={true}
+                  id="content"
+                  onChange={this.handleChange}
+                  style={{ height: 200 }}
+                  value={this.state.content}
+                />
+              </div>
+            </div>
+            <div className=" col s12 m6">
+              {/*<label htmlFor="password">MD Render</label>*/}
+              {/*<textarea*/}
+                {/*disabled={true}*/}
+                {/*className="materialize-textarea black-text"*/}
+                {/*id="content"*/}
+                {/*onChange={this.handleChange}*/}
+                {/*value="skip"*/}
+              {/*/>*/}
+              <div className="card">
+                <MarkdownRenderer markdown={this.state.content} />
+              </div>
+            </div>
           </div>
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Submit</button>
           </div>
-        </form>
+        </Form>
       </div>
     );
   }
@@ -57,7 +85,7 @@ const mapDispatchToProps = (dispatch) => {
 
 const mapStateToProps = (state) => {
   return {
-    ...state
+    auth: state.firebase.auth,
   }
 }
 
